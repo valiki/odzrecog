0a475bf386c6b348995dbfc05690167f738dd96d
Valiantsin Shukaila <vshukaila@exadel.com>
2013-03-25 00:47:58 +0300

    Spectral Angle CLusterer initial class added.

---
+++ image-recognition/src/main/java/by/sunnycore/recognition/image/cluster/impl/SpectralAngleClusterer.java
@@ -0,0 +1,19 @@
+package by.sunnycore.recognition.image.cluster.impl;
+
+import by.sunnycore.recognition.image.cluster.DataClusterer;
+
+/**
+ * Clusters data using Spectral Angle Clustering method
+ * 
+ * @author Valiantsin Shukaila
+ * 
+ */
+public class SpectralAngleClusterer implements DataClusterer {
+
+	@Override
+	public short[][][] cluster(short[][] data) {
+		
+		return null;
+	}
+
+}
